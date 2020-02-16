9ca4e35a5391e4dd9149a18ec6b15e106000b168
emmanuel <emmanuel.igwe38@gmail.com>
2020-02-16 04:39:32 +0100

    comparism op

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ async function getCourses() {
   //ni not in
   const courses = await Course
     //.find({ name: "igwe", isPublished: false })
-    .find({ price: { gte: 10 } })
+    //.find({ price: { $gte: 10, $lte: 20 } })
+    .find({ price: { $in: [10, 20, 30] } })
     .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
