f5ab2b9cff8142f47e0240ab20eb7bd96e9c133b
emmanuel <emmanuel.igwe38@gmail.com>
2020-02-21 01:44:39 +0100

    next commit

--- index.js
+++ index.js
@@ -42,10 +42,18 @@ async function getCourses() {
   const courses = await Course
     //.find({ name: "igwe", isPublished: false })
     //.find({ price: { $gte: 10, $lte: 20 } })
-    .find({ price: { $in: [10, 20, 30] } })
+    // .find({ price: { $in: [10, 20, 30] } })
+    //.find()
+    //.or([{ author: "Douglas" }, { isPublished: true }])
+    let pageNumber =2;
+    let pageSize =10;
+    .find({author: /^Mosh/})
+    .find({author: /hamodami$/})
+    .skip((pageNumber-1) * pageSize)
     .limit(10)
     .sort({ name: 1 })
-    .select({ name: 1, tags: 1 });
+    //.select({ name: 1, tags: 1 });
+    .count()
   console.log(courses);
 }
 
